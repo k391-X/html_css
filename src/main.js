36be1fc80d8e68d31f93e40c3619142d91f0282a
thang <cmthang2407@gmail.com>
2025-10-03 14:25:16 +0700

    update font chữ đúng

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import "./style.css";
 import "./custom.css";
+import "@fontsource/exo-2"; // mặc định weight 400
+import "@fontsource/exo-2/500.css"; // nếu cần weight 500
+import "@fontsource/exo-2/700.css"; // nếu cần weight 700
 import Layout from "./layout/Layout.js";
 import NewsList from "./pages/NewsList/index.js";
 import NewsDetail from "./pages/NewsDetail.js";
