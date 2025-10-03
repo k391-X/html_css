d7cd2f1993772e88779de14adb963318ddf9d0ed
Thang <cmthang2407@gmail.com>
2025-10-04 04:00:40 +0700

    update desktop interface

---
+++ src/components/NewsItem02_trang_tap_chi_dien_tu.js
@@ -0,0 +1,21 @@
+export default function NewsItem02({ image, date, title, desc, name }) {
+  return `
+    <div class="">
+      <!-- Hình (40%) -->
+      <div class="w-full h-[270px] p-3">
+        <img 
+          src="${image}" 
+          alt="demo" 
+          class="w-full h-full object-cover rounded-lg shadow custom-img-zoom" 
+        />
+      </div>
+
+      <!-- Nội dung (60%) -->
+      <div class="flex text-left text-[14px] font-[600] text-[#18364D] w-full px-3">
+        <div class="custom-link-underline title-news-item-txt !line-clamp-1" title="${name}">
+          ${name}
+        </div>
+      </div>
+    </div>
+  `;
+}
--- src/pages/NewsList/index.js
+++ src/pages/NewsList/index.js
@@ -1,68 +1,62 @@
-import Tag01 from "./components/tag_01.js";
-import Tag02 from "./components/tag_02_trang_chi_tiet_tin_tuc.js";
-import Tag03 from "./components/tag_03.js";
-import Tag04 from "./components/tag_04.js";
+import Tag01 from "./components/tag_01_tap_chi_dien_tu.js";
+import Tag02 from "./components/tag_02_tap_chi_dien_tu.js";
 import HomeIcon from "../../assets/home_icon.svg";
+import Banner from "../../assets/magazines/banner_tap_chi_dien_tu.png";
 
 export default function NewsList() {
   return `
     <main class="md:flex-row min-h-screen gap-4 px-4 pb-4 w-full">
 
       <!-- Phần thanh điều hướng - Ngay dưới header -->
-      <div class="bg-[#F5F4F4] w-full h-[2rem] flex items-center p-4 mb-2 h-full">
-        <img 
-          src="${HomeIcon}" 
-          alt="demo" 
-          class="cursor-pointer" 
-        />
+      <div class="relative">
+        <!-- Banner lớn phía sau -->
+        <div class="w-full">
+          <img 
+            src="${Banner}" 
+            alt="demo" 
+            class="" 
+          />
+        </div>
+        <div class="absolute z-10 top-0 left-6">
+          <div class="flex justify-start mt-6">
+            <span class="font-[700] text-xl md:text-[28px] text-[#0455A4]">
+              Tạp chí điện tử
+            </span>
+          </div>
+          <div class="w-full h-[2rem] flex items-center mt-3 h-full">
+            <img 
+              src="${HomeIcon}" 
+              alt="demo" 
+              class="cursor-pointer" 
+            />
 
-        <div class="px-[8px]">/</div>
+            <div class="px-[8px]">/</div>
 
-        <div class="text-[#2BBEF9]"> Tin tức </div>
+            <div class="text-[#2BBEF9]"> Tạp chí điện tử </div>
+          </div>
+        </div>
       </div>
 
-
-      <div class="flex">
-        <!-- Cột trái 70% trên desktop -->
-        <div class="w-full md:w-[70%] flex flex-col gap-4 overflow-y-auto">
+      <div class="flex mt-4">
+        <!-- Cột trái 30% trên desktop -->
+        <div class="w-full md:w-[30%] flex flex-col gap-4 overflow-y-auto pr-4">
           <div>
             ${Tag01()}
           </div>
-
-          <div class="flex justify-start">
-            <div class="font-[700] text-[28px] text-[#0455A4]">
-              Các bài đã đăng
-            </div>
-          </div>
-          <div>
-            ${Tag02()}
-          </div>
         </div>
 
-        <!-- Cột phải 30% trên desktop -->
-        <div class="w-full md:w-[30%] flex flex-col gap-4 overflow-y-auto">
+        <!-- Cột phải 70% trên desktop -->
+        <div class="w-full md:w-[70%] flex flex-col gap-4 overflow-y-auto">    
+            <div class="flex flex-col gap-4 overflow-y-auto">
+                <!-- Tiêu đề -->
+                <div class="flex justify-start">
+                    <span class="font-bold text-xl md:text-[28px] text-[#0455A4]">
+                        Danh sách tạp chí
+                    </span>
+                </div>
+            </div>      
           <div>
-            <div class="text-left title-txt uppercase">
-              Tin nổi bật
-            </div>
-          
-            <div class="border-[#2bbef9] border-t-[2px] mt-4"></div>
-          
-            <div class="">
-              ${Tag03()}
-            </div>
-          </div>
-          
-          <div>
-            <div class="text-left title-txt uppercase">
-              Tạp chí có nhiều lượt đọc
-            </div> 
-            
-            <div class="my-4 border-[#2bbef9] border-b-[2px]"></div>
-            
-            <div>
-              ${Tag04()}
-            </div>
+            ${Tag02()}
           </div>
         </div>
       </div>
