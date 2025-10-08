8d6d8a28a7c02db4ecf462241550fb0f55e4970a
thang <cmthang2407@gmail.com>
2025-10-08 09:18:06 +0700

    update interface gio hang

--- src/pages/NewsList/components/tag_02_gio_hang.js
+++ src/pages/NewsList/components/tag_02_gio_hang.js
@@ -6,20 +6,22 @@ export default function Tag02() {
     <div class="flex flex-col lg:flex-row items-start gap-6">
       <!-- Khối thông tin -->
       <div class="tag_02_right_chinh_sua_thong_tin py-4 px-2 w-full lg:w-2/3">
-        <div class="text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#0455A4] text-center lg:text-left">
-          Giỏ hàng của bạn
+        <div class="border-b border-[#DCDCDC]">
+          <div class="text-[20px] md:text-[22px] lg:text-[24px] font-[700] text-[#0455A4] text-center md:text-left pb-3 pl-4">
+            Giỏ hàng của bạn
+          </div>
         </div>
 
-        <div class="flex flex-col sm:flex-row sm:items-start mt-6 sm:mt-8 border-b border-[#E5E5E5] pb-4">
+        <div class="flex flex-col sm:flex-row sm:items-stretch mt-6 sm:mt-8 border-b border-[#DCDCDC] pb-4 px-4">
           <!-- Ảnh sản phẩm -->
-          <div class="w-[75px] sm:w-[100px] flex-shrink-0 mx-auto sm:mx-0">
-            <img src="${Image208}" alt="demo" class="w-full h-auto rounded-lg shadow custom-img-zoom" />
+          <div class="w-[75px] max-w-[75px] max-h-[110px] sm:w-[100px] flex-shrink-0 mx-auto sm:mx-0">
+            <img src="${Image208}" alt="demo" class="w-full h-auto rounded-lg custom-img-zoom" />
           </div>
 
           <!-- Thông tin sản phẩm -->
           <div class="mt-4 sm:mt-0 sm:ml-6 flex flex-col justify-between w-full">
             <div class="flex flex-col sm:flex-row sm:justify-between gap-2">
-              <div class="text-[14px] font-[600] text-[#484554] sm:max-w-[380px] text-center sm:text-left">
+              <div class="text-[14px] font-[600] text-[#484554] sm:max-w-[300px] text-center sm:text-left line-clamp-3">
                 Đánh giá độ tin cậy của hệ thống cách điện cho các động cơ điện có điện áp thấp được điều khiển bởi biến tần PWM
               </div>
 
@@ -37,13 +39,13 @@ export default function Tag02() {
               </div>
 
               <div class="max-w-[24px] max-h-[24px] cursor-pointer">
-                <img src="${IconDelete}" alt="demo" class="w-full h-auto rounded-lg shadow custom-img-zoom" />
+                <img src="${IconDelete}" alt="demo" class="w-full h-auto rounded-lg custom-img-zoom" />
               </div>
             </div>
           </div>
         </div>
 
-        <div class="mt-6 flex justify-center sm:justify-start">
+        <div class="mt-4 ml-4 flex justify-center sm:justify-start">
           <div class="custom-btn-buy">
             Mua thêm
           </div>
@@ -51,33 +53,38 @@ export default function Tag02() {
       </div>
 
       <!-- Khối thanh toán -->
-      <div class="w-full lg:w-1/3 max-w-[300px] text-left mt-6 lg:mt-0 mx-auto lg:mx-0 bg-[#F9F9F9] p-4 rounded-[5px]">
-        <div class="text-[18px] font-[700] text-[#0455A4] text-center lg:text-left">
-          Thông tin thanh toán
+      <div class="w-full lg:w-1/3 max-w-[300px] text-left mt-6 lg:mt-0 mx-auto lg:mx-0 bg-[#F9F9F9] p-4 tag_02_right_chinh_sua_thong_tin">
+        <div class="border-b border-[#DCDCDC]">
+          <div class="text-[18px] font-[700] text-[#0455A4] text-center lg:text-left pb-4 pt-1">
+            Thông tin thanh toán
+          </div>
         </div>
 
-        <div class="flex justify-between items-center mt-6">
-          <div class="text-[14px] font-[400] text-[#000000]">Tạm tính</div>
-          <div class="text-[14px] font-[400] text-[#000000]">72.000đ</div>
+        <div class="border-b border-[#DCDCDC]">
+          <div class="flex justify-between items-center mt-4">
+            <div class="text-[14px] font-[400] text-[#000000]">Tạm tính</div>
+            <div class="text-[14px] font-[400] text-[#000000]">72.000đ</div>
+          </div>
+
+          <div class="flex justify-between items-center mt-2">
+            <div class="text-[14px] font-[400] text-[#000000]">Giảm giá</div>
+            <div class="text-[14px] font-[400] text-[#000000]">0đ</div>
+          </div>
+
+          <div class="flex justify-between items-center mt-2">
+            <div class="text-[14px] font-[400] text-[#000000]">Phí vận chuyển</div>
+            <div class="text-[14px] font-[400] text-[#000000]">0đ</div>
+          </div>
+
+          <div class="mt-3 text-[13px] font-[400] text-[#656565] mb-3">
+            Đối với sách giấy, phí ship tính theo đơn (không giới hạn số lượng sách) theo khu vực: miền Bắc 20k, miền Trung 25k, miền Nam 35k
+          </div>
         </div>
 
-        <div class="flex justify-between items-center mt-2">
-          <div class="text-[14px] font-[400] text-[#000000]">Giảm giá</div>
-          <div class="text-[14px] font-[400] text-[#000000]">0đ</div>
-        </div>
 
-        <div class="flex justify-between items-center mt-2">
-          <div class="text-[14px] font-[400] text-[#000000]">Phí vận chuyển</div>
-          <div class="text-[14px] font-[400] text-[#000000]">0đ</div>
-        </div>
-
-        <div class="mt-3 text-[13px] font-[400] text-[#656565]">
-          Đối với sách giấy, phí ship tính theo đơn (không giới hạn số lượng sách) theo khu vực: miền Bắc 20k, miền Trung 25k, miền Nam 35k
-        </div>
-
-        <div class="flex justify-between items-center mt-3">
-          <div class="text-[14px] font-[400] text-[#000000]">Tổng cộng</div>
-          <div class="text-[16px] font-[700] text-[#0455A4]">72.000đ</div>
+        <div class="flex justify-between items-center mt-3 pb-4">
+            <div class="text-[14px] font-[400] text-[#000000]">Tổng cộng</div>
+            <div class="text-[16px] font-[700] text-[#0455A4]">72.000đ</div>
         </div>
 
         <div class="mt-4 text-[16px] font-[500] text-[#FFFFFF] bg-[#2BBEF9] text-center py-2 rounded-[5px] custom-btn-blue-login cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#1CA6DC] hover:scale-105 active:scale-95">
